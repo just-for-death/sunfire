227bcdc7e7c5a7a52ea0894163a7e0ad6aede18a
just-for-death <github.iag54@slmail.me>
2026-08-29 14:03:03 +0530

    UI Audit Implementations: Fixed Reader Page Restoration, added EmptyStates, added UX shortcuts and persistent sorting

--- lib/src/features/updates/updates_screen.dart
+++ lib/src/features/updates/updates_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:intl/intl.dart';
 
 import '../../core/db/isar_service.dart';
